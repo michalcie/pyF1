d1a1441043be6837daf5ff072eda367900eee46d
michalcie <michalcie4@gmail.com>
2021-10-08 20:54:28 +0200

    Further development of functionality (plot of point in/during season)

--- history_data.py
+++ history_data.py
@@ -10,8 +10,8 @@ soup = BeautifulSoup(source,'html.parser')
 table = soup.find_all('table')[0]
 df = pd.read_html(str(table), flavor='bs4', header=[0])[0]
 df.drop(["Unnamed: 0","Unnamed: 6"],axis=1, inplace=True)
-df.plot.bar(x="Driver", y="PTS");
-plt.show()
+# df.plot.bar(x="Driver", y="PTS");
+# plt.show()
 
 ### Data extraction for one race
 source = urllib.request.urlopen(f"https://www.formula1.com/en/results.html/1990/races/64/united-states/race-result.html").read()
@@ -27,7 +27,7 @@ def get_race_urls(year):
     soup = BeautifulSoup(source,'html.parser')
 
     for url in soup.find_all('a'):
-        if (str(year) in str(url.get('href'))) and ('race-result' in str(url.get('href'))) and (url.get('href') not in race_urls):
+        if (str(year) in str(url.get('href'))) and ('race-result.html' in str(url.get('href'))) and (url.get('href') not in race_urls):
             race_urls.append(url.get('href'))
     return race_urls
 
@@ -51,10 +51,11 @@ def seasons_results(race_urls):
             else:
                 race_name = race_name_temp
 
+        print(race_name)
         results_page = urllib.request.urlopen(f"{HOMEPAGE}{race}").read()
         race_results = BeautifulSoup(results_page,'html.parser')
-
-        table = race_results.find_all('table')[0]
+        if len(race_results.find_all('table')) > 0:
+            table = race_results.find_all('table')[0]
         df = pd.read_html(str(table), flavor='bs4', header=[0])[0]
         df.drop(["Unnamed: 0","Unnamed: 8"], axis=1, inplace=True)
         df.set_index('No', inplace=True)
@@ -70,8 +71,9 @@ def seasons_results(race_urls):
             # season_results_df.append([df['Driver'].loc[ind],df['Car'].loc[ind],*placeholder])
 
         for ind in df.index:
-            pts = df['PTS'].where(df.index == ind).dropna().values[0]
-            season_results_df.loc[ind, race_name] = int(pts)
+            if 'PTS' in df.columns:
+                pts = df['PTS'].where(df.index == ind).dropna()
+                season_results_df.loc[ind, race_name] = int(pts)
 
 
     #####Format the dataframe with a few lines#####
@@ -80,7 +82,21 @@ def seasons_results(race_urls):
     # season_results_df['Car'] = season_results_df['Car'].apply(lambda s : s[:3]).map(str.upper) #retain last 3 digits and caps
     return season_results_df
 
-race_urls = get_race_urls(2020)
+race_urls = get_race_urls(2021)
+print(race_urls)
 season_results_df = None
 results = seasons_results(race_urls)
 print(results)
+cum_results = results.drop(['Driver', 'Car'], axis=1).cumsum(axis=1)
+####configure dataframe for plotting####
+cum_results['Driver'] = results['Driver'].apply(lambda s : s[-3:]).map(str.upper)
+cum_results.set_index('Driver', inplace=True)
+cum_results.sort_values(by='austria',ascending=False, inplace=True)
+print(cum_results)
+
+
+cum_results.transpose().plot.line(figsize=(14,8),use_index=False,
+                                  xlim=(0,15)).legend(loc='center left',
+                                                      bbox_to_anchor=(1.0, 0.5))
+
+plt.show()
